ec2aa623775e1e875afcac94b5efc4a0e34c0b69
seriinn <144881847+seriinn@users.noreply.github.com>
2025-12-24 17:41:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class CustomBot(commands.Bot):
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = CustomBot(command_prefix='/', intents=intents)
+bot = CustomBot(command_prefix='!', intents=intents)
 
 # =====================
 # DB SETUP
